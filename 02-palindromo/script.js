849092db5734c2b284c99db657964460a694c40a
Rafalpv <rafaluperezvico@gmail.com>
2024-04-21 15:29:51 +0200

    Problema Palindromo Terminado

--- 02-palindromo/script.js
+++ 02-palindromo/script.js
@@ -1,26 +1,36 @@
-$inputText = document.querySelector('#text-input')
-$checkBtn = document.querySelector('#check-btn')
+document.addEventListener('DOMContentLoaded', function() {
+    const $inputText = document.querySelector('#text-input');
+    const $checkBtn = document.querySelector('#check-btn');
+    const $result = document.querySelector('#result');
 
+    $checkBtn.addEventListener('click', () => {
+        let word = $inputText.value;
+        if (word === '') {
+            alert('Please input a value');
+            return;
+        }
+        $inputText.value = '';
+        isPalindrome(word);
+    });
 
-$checkBtn.addEventListener('click', () => {
-    let word = $inputText.value
-    if (word === ''){
-        alert('Please input a value');
-        return
+    function isPalindrome(word) {
+        $result.replaceChildren();
+
+        let wordToCheck = word.replace(/[^A-Za-z0-9]/gi, '').toLowerCase()
+
+        let firstPart = wordToCheck.split('').slice(0, wordToCheck.length / 2).join('')
+        let secondPart = wordToCheck.split('').slice(wordToCheck.length % 2 === 0 ? wordToCheck.length / 2 : wordToCheck.length / 2 + 1, wordToCheck.length).reverse().join('')
+
+        console.log(firstPart);
+        console.log(secondPart);
+        
+        let resultMsg = `<strong>${word}</strong> ${firstPart === secondPart ? 'is' : 'is not'} a palindrome`;
+
+        const pTag = document.createElement('p');
+        pTag.className = 'user-input';
+        pTag.innerHTML = resultMsg;
+        $result.appendChild(pTag);
+
+        $result.classList.remove('hidden');
     }
-
-    $inputText.value = ''
-    isPalindrome(word)
-
-})
-
-function isPalindrome(word) {
-    let firstPart = word.split('').slice(0, word.length/2).join('')
-    let secondPart = word.split('').slice(word.length%2 === 0 ? word.length/2 : word.length/2+1, word.length).reverse().join('')
-
-    if (firstPart === secondPart)
-        alert('Palindrome')
-    else
-        alert('No Palindrome')
-
-}
+});
